2206184a29f0b63ddf546e11dc9c009781de67af
ashukr23450 <ashukr23450@gmail.com>
2024-07-28 22:50:13 +0530

    Filling the X and O

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 let boxes = document.querySelectorAll(".box");
 let resetBtn = document.querySelector("#reset-btn");
 
-let arr = [["tomato" , "KalaJamun"] , ["Apple" , "Orange"],["Banana" , "FishKari"]];
+let turnO = true; //for player 1 and player 2
+
+let winPattern = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];  // Chances to win
+
+boxes.forEach((box)=>{
+    box.addEventListener("click" ,()=>{
+      if(turnO)  
+    })
+})
