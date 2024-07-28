011089dc43898eac6c4675c0cbefde843c9549e3
ashukr23450 <ashukr23450@gmail.com>
2024-07-28 21:41:29 +0530

    changed arr

---
+++ script.js
@@ -0,0 +1,4 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset-btn");
+
+let arr = [["tomato" , "KalaJamun"] , ["Apple" , "Orange"],["Banana" , "FishKari"]];
