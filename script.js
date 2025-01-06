d85c14b0666da0588a92102a89243fab99cba766
ashukr23450 <ashukr23450@gmail.com>
2025-01-06 06:46:38 +0530

    js slected for winner check

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let boxes = document.querySelectorAll(".box");
 let resetBtn = document.querySelector("#reset-btn");
-
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
 let turnO = true ;
 
 let winPatterns = [
@@ -24,13 +25,26 @@ boxes.forEach((box)=>{
             turnO = true ;
         }
         box.disabled = true ;
+
+        checkWinner();
     });
-    checkWinner();
 });
 
-const checkWinner = ()=>{
+const checkWinner = () =>{
     for (let pattern of winPatterns){
-        console.log(pattern);
-                
-    };
-};
+        console.log(pattern[0],pattern[1],pattern[2]);
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
+
+        // Checking Winner pattern
+
+            if(pos1Val !="" && pos2Val !="" && pos3Val != ""){
+                if (pos1Val == pos2Val && pos2Val == pos3Val){
+                    console.log("Winner Decided" , pos3Val);
+            } else {
+                console.log ("Pattern is not matched");
+            }
+        }
+    }
+}
