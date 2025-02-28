c81d65f9aff4e2cf4d32df98437228fdf7b3b981
ASHUTOSH KUMAR <132243775+ashukr23450@users.noreply.github.com>
2025-02-28 18:55:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let msgContainer = document.querySelector(".msg-container");
 let msg = document.querySelector("#msg");
 let turnO = true ;
 
+
 let winPatterns = [
     [0,1,2],
     [3,4,5],
@@ -15,6 +16,7 @@ let winPatterns = [
     [0,4,8],
     [2,4,6]
 ];
+
 
 boxes.forEach((box)=>{
     box.addEventListener("click" , function (){
