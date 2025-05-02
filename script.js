2f68e22073e4b3a3fa3c1f2737f0d1c53438a27f
ASHUTOSH KUMAR <132243775+ashukr23450@users.noreply.github.com>
2025-05-03 02:31:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ let newGameBtn = document.querySelector("#new-btn");
 let msgContainer = document.querySelector(".msg-container");
 
 let msg = document.querySelector("#msg");
+
 let turnO = true ;
 
 
